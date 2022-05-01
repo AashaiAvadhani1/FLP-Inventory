4e79cc5738143d7fa9a5d6a8ce079c33f181d4f1
Tomas Viejobueno <tviejobu@andrew.cmu.edu>
2022-05-01 17:59:39 -0400

    Add outdated column to main website table

--- inventory/tables.py
+++ inventory/tables.py
@@ -31,7 +31,7 @@ class ItemTable(NameTable):
     class Meta:
         model = Item
         template_name = "django_tables2/bootstrap.html"
-        fields = ("id", "name", "category", "new_price", "used_price", "quantity", )
+        fields = ("id", "name", "category", "new_price", "used_price", "quantity", "outdated")
         order_by = 'name'
 
 class CheckinTable(tables.Table):   
